69dbecd28a2b628931b2366cab664e0c3a9d622f
Mfadillah21 <134469656+Mfadillah21@users.noreply.github.com>
2023-06-08 13:11:53 +0700

    Add files via upload

---
+++ tentang-saya.php
@@ -0,0 +1,32 @@
+<?php
+	include ('header.php');
+?>
+	<div class="col-sm-9">
+		<center><h2>Tentang Saya</h2></center>
+		<hr/>
+		<div class="well">
+		<center><p><b> Terima kasih telah berkunjung ke Web hasil Praktikum Pemrograman Web 1 </b></p></center>
+		</div>		
+<pre><h4>Nama		:  Muhammad Fadillah
+TTL		:  Gambah Dalam Barat, 21 Mei 200
+Alamat		:  Jl.Gambah Dalam Barat No.09 RT/RW 01/01 Kec. Kandangan
+		   Kab. Hulu Sungai Selatan, Prov. Kalimantan Selatan
+Kelas		:  Non Reguler BJB 4B
+</h4></pre>	
+	
+	</div>
+	
+	<div class="col-sm-3">
+		<div class="sidebar">
+		<center><img class="img" src="img/UNISKA.png" width="50%">
+		<h4><b>Universitas Islam Kalimantan Muhammad Arsyad Al Banjari</b></h4>
+		<hr/>
+			<div class="well">
+			<img class="img" src="img/4X6.jpg" width="85%">
+			</center>
+			</div>
+		</div>
+	</div>
+<?php
+	include ('footer.php');
+?>
